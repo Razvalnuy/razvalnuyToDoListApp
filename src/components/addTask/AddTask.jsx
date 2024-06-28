52eaec05e3ce9f28e5029868e1463784b2463905
Marat Gadjibutaev <megamg001@gmail.com>
2024-06-29 02:30:59 +0300

    added useMemo

--- src/components/addTask/AddTask.jsx
+++ src/components/addTask/AddTask.jsx
@@ -2,19 +2,17 @@ import { Button } from "@mui/material";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
-import { useContext, useState } from "react";
+import { useContext, useMemo, useState } from "react";
 import { TaskDispatchContext } from "../../context/taskContext";
 import { PlusIcon, TASK__TYPE } from "../../utils/variables";
 
 export default function AddTask() {
-
-
   const defaultText = "";
   const [text, setText] = useState(defaultText);
   const dispatch = useContext(TaskDispatchContext);
 
-  return (
-    <Box>
+  const title = useMemo(() => {
+    return (
       <Typography
         component="h2"
         color={"#2196F3"}
@@ -23,30 +21,38 @@ export default function AddTask() {
       >
         TODO
       </Typography>
-      <TextField
-        value={text}
-        onChange={(event) => setText(event.target.value)}
-        id="standard-basic"
-        label="Имя новой задачи"
-        variant="standard"
-        sx={{ width: "100%" }}
-        InputProps={{
-          endAdornment: (
-            <Button
-              disabled={!text.trim()}
-              onClick={() => {
-                setText(defaultText);
-                dispatch({
-                  type: TASK__TYPE.added,
-                  name: text,
-                });
-              }}
-            >
-              <PlusIcon />
-            </Button>
-          ),
-        }}
-      ></TextField>
+    );
+  }, []);
+
+  function addTaskClick(event) {
+    event.preventDefault();
+    setText(defaultText);
+    dispatch({
+      type: TASK__TYPE.added,
+      name: text,
+    });
+  }
+
+  return (
+    <Box>
+      {title}
+      <form onSubmit={addTaskClick}>
+        <TextField
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+          id="standard-basic"
+          label="Имя новой задачи"
+          variant="standard"
+          sx={{ width: "100%" }}
+          InputProps={{
+            endAdornment: (
+              <Button disabled={!text.trim()} onClick={addTaskClick}>
+                <PlusIcon />
+              </Button>
+            ),
+          }}
+        ></TextField>
+      </form>
     </Box>
   );
 }
